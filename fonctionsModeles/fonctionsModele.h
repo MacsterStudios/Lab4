249ee68369b15598c3bd9ceb7b532bb362e1a75d
MacsterStudios <geremy.sorlini.protic@gmail.com>
2015-03-03 10:40:31 -0500

    Created HPP files

--- fonctionsModeles/fonctionsModele.h
+++ fonctionsModeles/fonctionsModele.h
@@ -1,1 +1,10 @@
 #pragma once
+
+using namespace std;
+
+template <typename T>
+
+T TrouverMax(const T& tabT, const int nbrElements ) const;
+
+
+#include "fonctionsModele.hpp"
---
+++ fonctionsModeles/fonctionsModele.hpp
@@ -0,0 +1,8 @@
+using namespace std;
+
+template <typename T>
+
+T TrouverMax(const T& tabT, const int nbrElements) const{
+
+	return 1;
+}
